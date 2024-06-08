da7c32f0566e7a8cb4efdb8996f958bdd394d707
AbhishekSankhla121 <126145744+AbhishekSankhla121@users.noreply.github.com>
2024-06-08 23:44:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,16 +19,12 @@ config({
 });
 
 const app = express();
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true,
 }));
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/v1", course);
 app.use("/api/v1", user);
