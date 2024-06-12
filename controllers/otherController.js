856406e913a6073dfcfe1e7cea659d0e8599bfe5
AbhishekSankhla121 <126145744+AbhishekSankhla121@users.noreply.github.com>
2024-06-13 00:56:30 +0530

    Update otherController.js

--- controllers/otherController.js
+++ controllers/otherController.js
@@ -91,7 +91,7 @@ export const getDashboardStats = catchAsyncError(async (req, res, next) => {
         message: "",
         success: true,
         stats: StatsData,
-        usersCount,
+        userCount,
         subscriptionsCount,
         viewsCount,
         subscriptionsPercentage,
